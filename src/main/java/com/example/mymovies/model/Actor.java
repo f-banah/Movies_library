5d6ade4ec6b20783f136e6909ba0afae78c93395
Fathiya Banah <59838310+f-banah@users.noreply.github.com>
2021-12-02 15:07:20 +0000

    Add files via upload

---
+++ src/main/java/com/example/mymovies/model/Actor.java
@@ -0,0 +1,109 @@
+package com.example.mymovies.model;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name="actor")
+@JsonIdentityInfo(generator=ObjectIdGenerators.PropertyGenerator.class, property="id")
+public class Actor {
+
+    @Id
+    @Column(name="actor_id")
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name="actor_name")
+    private String name;
+
+    @Column(name="actor_surname")
+    private String surname;
+
+    @Column(name="actor_birth_date")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date date_of_birth;
+
+    @ManyToMany(mappedBy = "actors", fetch = FetchType.LAZY)
+    private Set<Movie> movies; // = new HashSet ???
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "country_id")
+    private Country country;
+
+    @OneToMany(mappedBy="actor")
+    private Set<ActorAward> awards;
+
+    public Actor() {
+        this.movies = new HashSet<>();
+        this.awards = new HashSet<>();
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public Date getDate_of_birth() {
+        return date_of_birth;
+    }
+
+    public void setDate_of_birth(Date date_of_birth) {
+        this.date_of_birth = date_of_birth;
+    }
+
+    public Set<Movie> getMovies() {
+        return movies;
+    }
+
+    public void setMovies(Set<Movie> movies) {
+        this.movies = movies;
+    }
+
+    public Country getCountry() {
+        return country;
+    }
+
+    public void setCountry(Country country) {
+        this.country = country;
+    }
+
+    public Set<ActorAward> getAwards() {
+        return awards;
+    }
+
+    public void setAwards(Set<ActorAward> awards) {
+        this.awards = awards;
+    }
+
+    @Override
+    public String toString() {
+        return this.name + " " + this.surname;
+    }
+}
---
+++ src/main/java/com/example/mymovies/model/Director.java
@@ -0,0 +1,109 @@
+package com.example.mymovies.model;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+import java.security.AlgorithmConstraints;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name="director")
+@JsonIdentityInfo(generator= ObjectIdGenerators.PropertyGenerator.class, property="id")
+public class Director {
+
+    @Id
+    @Column(name="director_id")
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name="director_name")
+    private String name;
+
+    @Column(name="director_surname")
+    private String surname;
+
+    @Column(name="director_birth_date")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date date_of_birth;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "country_id")
+    private Country country;
+
+    @OneToMany(mappedBy="director")
+    private Set<Movie> movies;
+
+    @OneToMany(mappedBy="director")
+    private Set<DirectorAward> awards;
+
+    public Director() {
+        this.awards = new HashSet<>();
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public Date getDate_of_birth() {
+        return date_of_birth;
+    }
+
+    public void setDate_of_birth(Date date_of_birth) {
+        this.date_of_birth = date_of_birth;
+    }
+
+    public Country getCountry() {
+        return country;
+    }
+
+    public void setCountry(Country country) {
+        this.country = country;
+    }
+
+    public Set<Movie> getMovies() {
+        return movies;
+    }
+
+    public void setMovies(Set<Movie> movies) {
+        this.movies = movies;
+    }
+
+    public Set<DirectorAward> getAwards() {
+        return awards;
+    }
+
+    public void setAwards(Set<DirectorAward> awards) {
+        this.awards = awards;
+    }
+
+    @Override
+    public String toString() {
+        return this.name + " " + this.surname;
+    }
+}
