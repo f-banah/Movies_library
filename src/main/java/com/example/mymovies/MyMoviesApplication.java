8cf4d7991d3801689958aff43343f2c0bfe4809a
Fathiya Banah <59838310+f-banah@users.noreply.github.com>
2021-12-02 15:31:12 +0000

    Add files via upload

---
+++ src/main/java/com/example/mymovies/MyMoviesApplication.java
@@ -0,0 +1,16 @@
+package com.example.mymovies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class MyMoviesApplication {
+
+    //TODO walą się błędy jak nie ma stworzonych tabel
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyMoviesApplication.class, args);
+    }
+
+}
